a8f1c165d21711f2a92dc09d3ffedcb9f311f6c4
Cl3Kener <you_didnt_see_me_here@hotmail.com>
2013-05-17 18:05:27 -0600

    Revert thermal changes

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -14,11 +14,11 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/cpufreq.h>
 #include <linux/mutex.h>
 #include <linux/msm_tsens.h>
 #include <linux/workqueue.h>
 #include <linux/cpu.h>
-<<<<<<< HEAD
 #include <linux/reboot.h>
 
 #define DEF_TEMP_SENSOR0      0
@@ -52,16 +52,6 @@ static int thermal_throttled = 0;
 //Save the cpu max freq before throttling
 static int pre_throttled_max = 0;
 
-=======
-#include <linux/cpufreq.h>
-#include <linux/msm_tsens.h>
-#include <linux/msm_thermal.h>
-#include <mach/cpufreq.h>
-
-static int enabled;
-static struct msm_thermal_data msm_thermal_info;
-static uint32_t limited_max_freq = MSM_CPUFREQ_NO_LIMIT;
->>>>>>> cm-10.1
 static struct delayed_work check_temp_work;
 static struct workqueue_struct *check_temp_workq;
 
@@ -99,37 +89,27 @@ static struct msm_thermal_tuners {
 	.check_interval_ms = DEF_THERMAL_CHECK_MS,
 };
 
-static int update_cpu_max_freq(int cpu, uint32_t max_freq)
+static int update_cpu_max_freq(struct cpufreq_policy *cpu_policy,
+			       int cpu, int max_freq)
 {
 	int ret = 0;
 
-	ret = msm_cpufreq_set_freq_limits(cpu, MSM_CPUFREQ_NO_LIMIT, max_freq);
-	if (ret)
-		return ret;
+	if (!cpu_policy)
+		return -EINVAL;
+
+	cpufreq_verify_within_limits(cpu_policy,
+				cpu_policy->min, max_freq);
+	cpu_policy->user_policy.max = max_freq;
 
 	ret = cpufreq_update_policy(cpu);
-<<<<<<< HEAD
 	if (!ret)
 		pr_debug("msm_thermal: Setting CPU%d max frequency to %d\n",
 			cpu, max_freq);
-=======
-	if (ret)
-		return ret;
-
-	limited_max_freq = max_freq;
-	if (max_freq != MSM_CPUFREQ_NO_LIMIT)
-		pr_info("msm_thermal: Limiting cpu%d max frequency to %d\n",
-				cpu, max_freq);
-	else
-		pr_info("msm_thermal: Max frequency reset for cpu%d\n", cpu);
-
->>>>>>> cm-10.1
 	return ret;
 }
 
 static void check_temp(struct work_struct *work)
 {
-<<<<<<< HEAD
 	struct cpufreq_policy *cpu_policy = NULL;
 	struct tsens_device tsens_dev0;
 	struct tsens_device tsens_dev1;
@@ -260,68 +240,33 @@ static void check_temp(struct work_struct *work)
 			update_cpu_max_freq(cpu_policy, cpu, max_freq);
 
 		cpufreq_cpu_put(cpu_policy);
-=======
-	struct tsens_device tsens_dev;
-	unsigned long temp = 0;
-	uint32_t max_freq = limited_max_freq;
-	int cpu = 0;
-	int ret = 0;
-
-	tsens_dev.sensor_num = msm_thermal_info.sensor_id;
-	ret = tsens_get_temp(&tsens_dev, &temp);
-	if (ret) {
-		pr_debug("msm_thermal: Unable to read TSENS sensor %d\n",
-				tsens_dev.sensor_num);
-		goto reschedule;
-	}
-
-	if (temp >= msm_thermal_info.limit_temp)
-		max_freq = msm_thermal_info.limit_freq;
-	else if (temp <
-		msm_thermal_info.limit_temp - msm_thermal_info.temp_hysteresis)
-		max_freq = MSM_CPUFREQ_NO_LIMIT;
-
-	if (max_freq == limited_max_freq)
-		goto reschedule;
-
-	/* Update new limits */
-	for_each_possible_cpu(cpu) {
-		ret = update_cpu_max_freq(cpu, max_freq);
-		if (ret)
-			pr_debug("Unable to limit cpu%d max freq to %d\n",
-					cpu, max_freq);
->>>>>>> cm-10.1
 	}
 
 reschedule:
 	if (enabled)
-<<<<<<< HEAD
 		queue_delayed_work(check_temp_workq, &check_temp_work,
 				msecs_to_jiffies(msm_thermal_tuners_ins.check_interval_ms));
         return;
-=======
-		schedule_delayed_work(&check_temp_work,
-				msecs_to_jiffies(msm_thermal_info.poll_ms));
->>>>>>> cm-10.1
 }
 
 static void disable_msm_thermal(void)
 {
 	int cpu = 0;
-
-	/* make sure check_temp is no longer running */
-	cancel_delayed_work(&check_temp_work);
-	flush_scheduled_work();
-
-	if (limited_max_freq == MSM_CPUFREQ_NO_LIMIT)
-		return;
+	struct cpufreq_policy *cpu_policy = NULL;
 
 	/* make sure check_temp is no longer running */
 	cancel_delayed_work_sync(&check_temp_work);
 	flush_scheduled_work();
 
 	for_each_possible_cpu(cpu) {
-		update_cpu_max_freq(cpu, MSM_CPUFREQ_NO_LIMIT);
+		cpu_policy = cpufreq_cpu_get(cpu);
+		if (cpu_policy) {
+			if (cpu_policy->max < cpu_policy->cpuinfo.max_freq)
+				update_cpu_max_freq(cpu_policy, cpu,
+						    cpu_policy->
+						    cpuinfo.max_freq);
+			cpufreq_cpu_put(cpu_policy);
+		}
 	}
 }
 
@@ -348,7 +293,6 @@ static struct kernel_param_ops module_ops = {
 module_param_cb(enabled, &module_ops, &enabled, 0644);
 MODULE_PARM_DESC(enabled, "enforce thermal limit on cpu");
 
-<<<<<<< HEAD
 /**************************** SYSFS START ****************************/
 struct kobject *msm_thermal_kobject;
 
@@ -560,18 +504,10 @@ static struct attribute_group msm_thermal_attr_group = {
 /**************************** SYSFS END ****************************/
 
 static int __init msm_thermal_init(void)
-=======
-int __init msm_thermal_init(struct msm_thermal_data *pdata)
->>>>>>> cm-10.1
 {
 	int rc, ret = 0;
 
-	BUG_ON(!pdata);
-	BUG_ON(pdata->sensor_id >= TSENS_MAX_SENSORS);
-	memcpy(&msm_thermal_info, pdata, sizeof(struct msm_thermal_data));
-
 	enabled = 1;
-<<<<<<< HEAD
         check_temp_workq = alloc_workqueue(
                 "msm_thermal", WQ_UNBOUND | WQ_RESCUER, 1);
         if (!check_temp_workq)
@@ -595,10 +531,3 @@ fs_initcall(msm_thermal_init);
 
 
 
-=======
-	INIT_DELAYED_WORK(&check_temp_work, check_temp);
-	schedule_delayed_work(&check_temp_work, 0);
-
-	return ret;
-}
->>>>>>> cm-10.1
